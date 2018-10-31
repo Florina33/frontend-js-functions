f9b3f03bc2a1160b0476021bf261043b12cba8dd
Svetlana <florina33@gmail.com>
2018-10-31 17:23:36 +0200

    remove comments and old code, fix ESLint errors and warnings

--- src/index.js
+++ src/index.js
@@ -1,116 +1,63 @@
+/** ************ function MinNumber **************/
 
-/******* TASK 1 *******/
-
-var x = 6;
-var y = 15;
-var z = 4;
-var res;
-
-res = x+=y-x++*z;			
-console.log('res', res);	// -3
-
-res = z=--x-y*5;
-console.log('res', res);	// -79 
-
-res = y/=x+5%z; 			
-console.log('res', res);	//  15
-
-res = z=x++ +y*5;			
-console.log('res', res);	// 71   
-
-res = x=y-x++*z;			
-console.log('res', res);	// 228
-
-
-/************** function MinNumber **************/
-
-function isNumber(args) {
+function isNumber (args) {
 	let arr = [];
 
 	for (let i = 0; i < args.length; i++) {
 		arr[i] = args[i];
 	}
-	let arrNum = arr.filter(item => typeof item == 'number');
+	let arrNum = arr.filter(item => typeof item === 'number');
 
 	return arrNum;
 }
 
-/***/
-
-export function min(arr) {
-	if(!arr){
+export function min (arr) {
+	if (!arr) {
 		return;
 	}
-	var arrNum = isNumber(arr);
-	if(!arrNum.length){
+	let arrNum = isNumber(arr);
+
+	if (!arrNum.length) {
 		return;
 	}
-	
-	/*
-	var min = arrNum[0];
-	var i;
-	for(i=0; i < arrNum.length; i++) {
-		if(arrNum[i] < min)
-		min = arrNum[i];
-	}*/
-	// or
-	var min = arrNum.reduce((min, current) => (min < current ? min : current));
 
-	return min;
+	let minItem = arrNum.reduce((minCurrent, current) => (minCurrent < current ? minCurrent : current));
+
+	return minItem;
 }
 
-/************** function MaxNumber **************/
+/** ************ function MaxNumber **************/
 
-export function max(arr) {
-	if(!arr){
+export function max (arr) {
+	if (!arr) {
 		return;
 	}
-	var arrNum = isNumber(arr);
-	if(!arrNum.length){
+	let arrNum = isNumber(arr);
+
+	if (!arrNum.length) {
 		return;
 	}
 
-	/*
-	var max = arrNum[0];
-	var i;
-	for(i=0; i < arrNum.length; i++) {
-		if(arrNum[i] > max)
-			max = arrNum[i];
-	}*/
-	// or
-	var max = arrNum.reduce((max, current) => (max > current ? max : current));
+	let maxItem = arrNum.reduce((maxCurrent, current) => (maxCurrent > current ? maxCurrent : current));
 
-	return max;
+	return maxItem;
 }
 
-/************** function SumNumbers **************/
+/** ************ function SumNumbers **************/
 
-export function sum(...args) {
-	if(!arguments){
+export function sum () {
+	if (!arguments) {
+		return 0;
+	}
+	let arrNum = isNumber(arguments);
+
+	if (!arrNum.length) {
 		return 0;
 	}
 
-	let arrNum = isNumber(arguments);	
-	if(!arrNum.length){
-		return 0;
-	}
+	let Sum = 0;
 
-	let sum = 0;
+	Sum = arrNum.reduce((sumItems, current) => sumItems + current);
 
-	/*
-	var i; 
-	for(i=0; i<arrNum.length; i++) {
-		if(typeof arrNum[i] == "number")
-			sum += arrNum[i];
-	}*/
-	// or
-	sum = arrNum.reduce((sumItems, current) => sumItems + current);
-
-	return sum;
+	return Sum;
 }
-
-// export { min, max, sum };
-// setInterval(function () {}, 10);
-
-
-
