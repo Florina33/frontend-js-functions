cfef21f5dfc5517a6b82a7efb24536e4c05c9d54
Svetlana <florina33@gmail.com>
2018-11-06 02:27:21 +0200

    remove check in the functions if(!arrNumbers) and remove comments

--- src/index.js
+++ src/index.js
@@ -16,38 +16,20 @@ function getSum (previousValue, currentValue) {
 
 /*********/
 
-function min (arrNumbers = []) {
-	if (!arrNumbers) {return undefined;}
-
+export function min (arrNumbers = []) {
 	return arrNumbers
 		.filter(isNumber)
 		.reduce(getLess, undefined);
 }
 
-function max (arrNumbers = []) {
-	if (!arrNumbers) {return undefined;}
-
+export function max (arrNumbers = []) {
 	return arrNumbers
 		.filter(isNumber)
 		.reduce(getGreater, undefined);
 }
 
-function sum (...arrNumbers) {
-	if (!arrNumbers) {return 0;}
-
-	return arrNumbers.filter(isNumber)
+export function sum (...arrNumbers) {
+	return arrNumbers
+		.filter(isNumber)
 		.reduce(getSum, 0);
 }
-// console.log( sum([88, 22, -15]) ); // gives the result: 0
-
-// The correct result when using array arrNumbers[0]
-/*
-function sum (...arrNumbers) {
-	if (!arrNumbers[0]) {return 0;}
-
-	return arrNumbers[0].filter(isNumber)
-		.reduce(getSum, 0);
-}
-*/
-
-export { min, max, sum };
